aa17b0bf5908e53121d28fa6de0b1147965c4968
Radivarig <reslav.hollos@gmail.com>
2015-06-26 04:55:01 +0200

    (add) PopupLink and example text

--- src/entry.jsx
+++ src/entry.jsx
@@ -3,44 +3,82 @@ var Popups = require('./Popups.jsx')
 
 var Popup = React.createClass({
   render: function() {
-    var s = {
-      height: '100'
-    , width: '170'
+    var popup_style = {
+      height: 'auto'
+    , width: 'auto'
     , backgroundColor: '#ABC'
     , borderStyle: 'solid'
     , borderColor: '#567'
     }
     var data = this.props.data || ''
+    var ToDisplay = <DefaultPopup info={data.nodeValue}/>
+    if (data.nodeValue === 'universe') {ToDisplay = <Universe/>}
+
     return (
-      <div style={s}>
-        data: {this.props.data.nodeValue}
+      <div style={popup_style}>
+        {ToDisplay}
+      </div>
+    )
+  }
+})
+
+var PopupLink = React.createClass({
+  render: function(){
+    var link_style = {
+      cursor: 'pointer'
+      , color: '#00F'
+    }
+    return (
+      <span data={this.props.data} style={link_style}>
+        {this.props.children}
+      </span>
+    )
+  }
+})
+
+var DefaultPopup = React.createClass({
+  render: function(){
+    return (
+      <div >
+        <div>info: {this.props.info}</div>
+        <div>other:</div>
         <ul>
-          <li><div data={'some popup data1'}>popup data 1</div></li>
-          <li><div data={'some popup data2'}>popup data 2</div></li>
+          <li><PopupLink data='universe'>Universe</PopupLink></li>
+          <li><PopupLink data='planets'>planets</PopupLink></li>
+          <li><PopupLink data='stars'>stars</PopupLink></li>
+          <li><PopupLink data='galaxies'>galaxies</PopupLink></li>
+          <li><PopupLink data='intergalactic space'>intergalactic space</PopupLink></li>
+          <li><PopupLink data='dark matter'>dark matter</PopupLink></li>
+          <li><PopupLink data='dark energy'>dark energy</PopupLink></li>
         </ul>
       </div>
     )
   }
 })
 
+var Universe = React.createClass({
+  render: function() {
+    return (
+      <p>
+        The <PopupLink data={'universe'}>Universe</PopupLink> is all of time and space and its contents.
+        The Universe includes <PopupLink data={'planets'}>planets</PopupLink>
+        , <PopupLink data={'stars'}>stars</PopupLink>
+        , <PopupLink data={'galaxies'}>galaxies</PopupLink>
+        , the contents of <PopupLink data={'intergalactic space'}>intergalactic space</PopupLink>
+        , the smallest subatomic particles, and all matter and energy.
+        The majority of matter and energy is most likely in the form
+        of <PopupLink data={'dark matter'}>dark matter</PopupLink> and <PopupLink data={'dark energy'}>dark energy</PopupLink>.
+      </p>
+    )
+  }
+})
+
 var App = React.createClass({
-  render: function(){
-    var s = {
-      borderStyle: 'solid'
-    , borderColor: '#234'
-    , borderWidth: '1px'
-    , textAlign: 'center'}
+  render: function() {
     return (
       <div>
-        
         <Popups handler={Popup} data={'attributes.data'}/>
-        
-        <ul>
-          <li style={s}><div data={'custom data1'}>data1</div></li>
-          <li style={s}><div data={'custom data2'}>data2</div></li>
-          <li style={s}><div data={'custom data3'}>data3</div></li>
-        </ul>
-      
+        <Universe/>
       </div>
     )
   }
