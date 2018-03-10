ac45443d0a8242580602a34d00aa9228acf788d3
Radivarig <reslav.hollos@gmail.com>
2018-03-10 04:12:25 +0100

    Rename to match webpack paths

--- src/index.js
+++
@@ -1,5 +0,0 @@
-import React from 'react'
-import ReactDOM from 'react-dom'
-import PopupsViewer from './PopupsViewer.jsx'
-
-ReactDOM.render(<PopupsViewer/>, document.getElementById('app'))
---
+++ src/viewer.js
@@ -0,0 +1,5 @@
+import React from 'react'
+import { render } from 'react-dom'
+import Viewer from './Viewer.jsx'
+
+render(<Viewer/>, document.getElementById('app'))
